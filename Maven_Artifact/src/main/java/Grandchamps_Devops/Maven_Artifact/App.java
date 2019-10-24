61f4b84735a8ad87f8dd518e0cf93bd3e4886b7f
deemoniak <corentin.grandchamps@gmail.com>
2019-10-24 16:33:40 +0200

    Add Maven, Sonar, and begin Jenkins

---
+++ Maven_Artifact/src/main/java/Grandchamps_Devops/Maven_Artifact/App.java
@@ -0,0 +1,13 @@
+package Grandchamps_Devops.Maven_Artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
